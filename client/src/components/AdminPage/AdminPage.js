194f513312fd5f1a6628f00728bd9f68a1d7e3ca
natirey619 <natirey619@gmail.com>
2023-02-17 03:08:53 +0200

    canworkfromhome

--- client/src/components/AdminPage/AdminPage.js
+++ client/src/components/AdminPage/AdminPage.js
@@ -1,54 +1,19 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react';
-import "./AdminPage.css"
+import React, { useState } from "react";
+import { useEffect } from "react";
+import "./AdminPage.css";
+import AdminGetUsers from "./AdminPages/AdminGetUsers";
 
 const AdminPage = () => {
-  const [allUsers, setAllUsers] = useState('');
-  const allPulledUsers = []
-
-  const getAllUsers = async () => {
-    try {
-      const respone = await fetch(
-        "http://localhost:8000/api/users/getAllUsers"
-      );
-      const data = await respone.json();
-      setAllUsers(data);
-      console.log(allUsers)
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
   // useEffect(() => {
   //   getAllUsers();
   // }, []);
 
   return (
-
     <div>
       <h1>admin page</h1>
-      <div className="get-all-users">
-        <button
-          onClick={getAllUsers}
-        >get all users</button>
-         {allUsers.map
-          ? allUsers.map(user => 
-          <p>{user.firstName}</p>
-          
-          
-          
-          )
-          : 'Nothing to show...'
-        }
-        
-        </div>
+      <AdminGetUsers />
+    </div>
+  );
+};
 
-      
-      
-      
-      
-      </div>
-  )
-}
-
-export default AdminPage
+export default AdminPage;
